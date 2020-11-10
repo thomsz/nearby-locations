55b78e1d69f0ce4e94d138860cf1b6b3885494ff
Tom Segev <hi@tomsegev.com>
2020-11-10 15:59:20 +0200

    Add basic search field

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState } from 'react';
+import { Input } from 'antd';
 import './App.css';
 
-function App() {
-	return <div className="App"></div>;
-}
+const { Search } = Input;
+
+const App = () => {
+	const [searchQuery, setSearchQuery] = useState('');
+
+	const onSearch = (input) => {
+		setSearchQuery(input);
+		console.log('submitted', input);
+	};
+
+	return (
+		<div className="App">
+			<Search
+				placeholder="input search text"
+				onSearch={onSearch}
+				style={{ width: 200 }}
+			/>
+			<h2>{searchQuery}</h2>
+		</div>
+	);
+};
 
 export default App;
